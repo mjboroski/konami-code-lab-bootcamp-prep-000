1446e93c96111ac6937489c9b44e229cca17c1d6
Michael Boroski <mjboroski@gmail.com>
2018-04-05 19:41:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  index.addEventListener('keydown', function(e){
+  document.addEventListener('keydown', function(e){
     const key = e.which;
 
     if (key === code[index]) {
