eb8c1586ae2e70d7179042af7466508cbb617101
Michael Boroski <mjboroski@gmail.com>
2018-04-05 19:23:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,6 +4,7 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   index.addEventListener('keydown', onKeyDownHandler());
+}
   // This is the function that would be invoked by the event listener.
 function onKeyDownHandler() {
   const key = parseInt(e.which);
